99ba0ea8af18eba1fbf13ec9af77cad4a22fa13a
Articulate Logic <articulatelogic@shared.sforge.com>
2015-12-08 21:43:47 +0100

    production branch with settings

--- quran_cl/settings.py
+++ quran_cl/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'quran_db',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'articulatelogic',
+        'PASSWORD': 'pass#100',
         'HOST': 'localhost',
         'PORT': '3306',
     }
